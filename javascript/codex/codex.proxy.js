ca52396d99ccce45f36f71b09947094ad98c3483
Milan Jaric <milan.jaric@gmail.com>
2011-11-16 13:43:50 +0100

    json Proxy

---
+++ javascript/codex/codex.proxy.js
@@ -0,0 +1,99 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: Milan Jaric
+ * Date: 11/16/11
+ * Time: 1:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Always pass a root json
+ * @param rootObj json
+ * @param path how dep we are in recursion, by default this is empty string and it is in form "propertyName1.property_name2.leaf"
+ */
+var Proxy = function(rootObj, path) {
+    var _beforeCallbacks = $.Callbacks("stopOnFalse");
+    var _afterCallbacks = $.Callbacks("stopOnFalse");
+    var _path = (path || "").split(".");
+    var _root = rootObj || {};
+
+    //for closure purpose
+    var _proxy = this;
+
+    function _getReference(obj, path) {
+        var root = obj;
+        var ref = null;
+        var accessPath = path;
+        if (typeof path === 'string') {
+            accessPath = path.split(".");
+        }
+        var evalScript = "ref = root" + "['";
+        evalScript += accessPath.join("'].['");
+        evalScript += "'];";
+        return ref;
+    }
+
+    function __changing__(e) {
+        _beforeCallbacks.fire(e);
+    }
+
+    function __changed__(e) {
+        _afterCallbacks.fire(e);
+    }
+
+    for (var propertyName in rootObj) {
+        // this will handle array to but keep iin mind that path will be property.1...
+        // index will be appended at the current iteration level
+        var currentReference = _getReference(_root, _path.push(propertyName));
+
+
+        if ($.isArray(currentReference)) {
+
+
+        } else if (typeof currentReference in {'string':1, 'number':1, 'boolean':1}) {
+
+            this.__defineGetter__(propertyName, function() {
+
+            });
+            this.__defineSetter__(propertyName, function() {
+
+            });
+
+        } else {
+
+            // it is object (sub json)
+            this.__defineGetter__(propertyName, (function(path) {
+                var currentPath = path;
+                var proxy = new Proxy(_root, currentPath);
+                // notify when property is changing
+                proxy.onChanged(__changed__);
+                proxy.onChanging(__changing__);
+                return  function() {
+                    return proxy;
+                }
+            })(_path));
+            // Todo: Check if there is case when we want to bind different property
+        }
+
+    }
+
+    this.onChanging = function(callback) {
+        _beforeCallbacks.add(callback);
+        return this;
+    }
+
+    this.onChanged = function(callback) {
+        _afterCallbacks.add(callback);
+        return this;
+    }
+
+};
+
+var ArrayProxy = function (rootObj, path) {
+    var _fakeArray = {};
+    for (var idx in obj) {
+        _fakeArray.__defineGetter__(idx.toString(), function() {
+
+        })
+    }
+}
---
+++ javascript/codex/core.js
@@ -0,0 +1,23 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: Milan Jaric
+ * Date: 11/16/11
+ * Time: 12:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+(function(){
+
+    var codex = new function codex(){
+
+        this.version = "0.1";
+
+        
+    }();
+
+    window.application = codex;
+
+
+    
+
+})();
